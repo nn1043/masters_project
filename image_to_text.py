7d28b864f45dda262df18bab0fc405de9ca18006
nn1043 <55171942+nn1043@users.noreply.github.com>
2022-01-23 16:17:31 -0500

    Add files via upload

---
+++ image_to_text.py
@@ -0,0 +1,16 @@
+import base64
+
+with open("image.png", "rb") as image:
+    image_64 = base64.b64encode(image.read())
+    image_bits = base64.b64decode(image_64)
+    image_binary = "".join(["{:08b}".format(x) for x in image_bits])
+    with open("text_binary.txt", "w") as text_binary:
+        new_line = 0
+        for i in image_binary:
+            text_binary.write(i)
+            new_line += 1
+            if new_line >= 8:
+                text.write("\n")    # can be used to compare lines of binary
+                new_line = 0
+    with open("text_base64.txt", "wb") as text_base64:
+        text_base64.write(image_64)
